bd1bd1c82894ab3c841804ee429e3d84423dfa8c
Anton Aksionau <ikari808@gmail.com>
2015-08-03 20:00:58 +0400

    second second second commit

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 var locations=[];
 //var strike=newDiv.getAttribute("data");
-
+var NumOfCell;
 var done=0;
 var miss;
 var fire;
 
-for(i=0; i<=10; i++) {
+for(i=0; i<=9; i++) {
 	var cell=i;
 	locations[i]=i;
 	var newDiv=document.createElement("div");
@@ -15,25 +15,27 @@ for(i=0; i<=10; i++) {
 	document.body.appendChild(newDiv);
 	
 };
+alert(newDiv[1]);
 
 $(".loc").click(function(){
-
-	var NumOfCell=$(this).attr("data");
-	alert(NumOfCell);
-
+	NumOfCell=$(this).attr("data");
+	getPlay();
 });
 
+alert(NumOfCell);
+
+
 var randLoc=Math.floor((Math.random()*10)+1);
-//alert(randLoc);
+
 locations[randLoc]="ship";
 locations[randLoc+1]="ship";
-//locations[1]="ship";
-outer: while(done<2){
-	//alert("anus");
+function getPlay(){
+//outer: while(done<2){
+	//alert("while is active");
 	//strike=prompt("try again enter num of cell ",""); //@!!!!!!
 
 for(var key in locations){
-	if(locations[key]=="ship" && key==strike){
+	if(locations[key]=="ship" && key==NumOfCell){
 		/*alert("strike");
 		done++;
 		if (done==2) {alert("you win") };
@@ -41,7 +43,8 @@ for(var key in locations){
 		var strike=prompt("enter num of cell","");
 		//continue*/
 		fire=true;
-		locations[key]="deadShip"
+		locations[key]="deadShip";
+		newDiv.className="deadShip";
 		console.log(locations[key]);
     }
     else{
@@ -56,12 +59,12 @@ for(var key in locations){
 if (fire===true) {
 	alert("strike");
 	var color=document.getElementsByTagName('div');
-	$(".loc").css("background", "red");
+	//$(".loc").css("background", "red");
 	document.body.style.backgroundColor = 'red';
 	//color.style.backgroundColor='blue';
 	setTimeout( function() {
 		document.body.style.background = "";
-	}, 100);
+	}, 200);
 	
 	done++;
 	fire=false;
@@ -69,20 +72,25 @@ if (fire===true) {
 	//strike=prompt("try again enter num of cell ","");	
 	
 		//if(done==2){alert("you win")};	
-	continue outer
+	//continue outer
 
     }
     else{
     	alert("miss");
     	//strike=prompt("try again enter num of cell ","");		
     	miss=false;
-	    continue outer
+	    //continue outer
 
     };
 
-};//for
-alert("you win")
-//alert(locations[3]);
+//};//while
+	if (done==2) {
+		alert("you win");
+
+
+	};
+
+};//getPlay
    
 
 
